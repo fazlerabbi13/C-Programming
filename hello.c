d51c0db12651161b261e86aa8c01bae487b797d8
fazlerabbi13 <mohammad0132794@gmail.com>
2024-09-22 23:38:22 +0600

    practice variable

---
+++ hello.c
@@ -0,0 +1,24 @@
+// first program in c
+#include <stdio.h>
+
+int main() {
+    printf("hello world \n");
+    printf("keep going fazle rabbi.don't care upcamming problems");
+    
+    int myNum = 10;
+    float myFloat = 10.0;
+    char myLetter = 's';
+
+    printf("%d\n", myNum);
+    printf("%f\n", myFloat);
+    printf("%c", myLetter);
+    return 0;
+
+    return 0;
+}
+
+//variable
+
+
+
+    

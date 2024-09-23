59dfa1ec2266a70dde9e6514e3871bc7619a6b24
fazlerabbi13 <mohammad0132794@gmail.com>
2024-09-23 17:13:42 +0600

    calculate  rectangle area

---
+++ caculateArea.c
@@ -0,0 +1,15 @@
+
+// Calculate rectangle area
+#include <stdio.h>
+
+int main() {
+    int length = 5;
+    int width = 4;
+    int area;
+
+    area = length * width;
+
+    printf("length is:%d\n",length);
+    printf("width is:%d\n",width);
+    printf("Area is:%d",area);
+}

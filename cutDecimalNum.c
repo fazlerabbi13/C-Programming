dbe2862de892f404295d623728b3a277ad4a0f8a
fazlerabbi13 <mohammad0132794@gmail.com>
2024-09-24 12:47:51 +0600

    cutting decimal number

---
+++ cutDecimalNum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    float myFloat = 4.5;
+
+    printf("%f\n", myFloat);
+    printf("%.1f\n", myFloat);
+    printf("%.2f\n", myFloat);
+    printf("%.3f", myFloat);
+
+}

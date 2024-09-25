37a12e7878216b048002452d5e0d41303a777d7f
fazlerabbi13 <mohammad0132794@gmail.com>
2024-09-25 14:30:13 +0600

    nested loop

--- forLoop.c
+++ forLoop.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int i;
+    int i,k;
+    // outer
     for(i = 0; i<=4; i++) {
-        printf("%d\n", i);
+        printf("outer:%d\n", i);
+    // inner
+        for(k = 1; k<=4; k++){
+            printf("inner:%d\n", k);
+        }
     }
     return 0;
 }

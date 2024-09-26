84434138daabbc7e28402115a6390cf957874cb4
fazlerabbi13 <mohammad0132794@gmail.com>
2024-09-26 11:50:45 +0600

    user input

---
+++ userInput/userInput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char name[30];
+
+    printf("Enter your name and press enter:\n");
+    scanf("%s", name);
+    printf("hello %s", name);
+
+    return 0;
+
+}
